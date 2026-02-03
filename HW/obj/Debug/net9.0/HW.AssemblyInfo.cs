ca4b6a5e71d28ed2a942e84b7c7e1b50ebd2751e
VladislaV9 <vlad.nikitenkov95@gmail.com>
2026-02-03 12:59:25 +0300

    new

--- HW/obj/Debug/net9.0/HW.AssemblyInfo.cs
+++ HW/obj/Debug/net9.0/HW.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+963b23b05cb4f7c00af4307534d603d5a43f52c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f249e2a9afc6d7ca6ea541ac29645234897583e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
